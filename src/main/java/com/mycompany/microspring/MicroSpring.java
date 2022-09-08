375b6c3539a73936b5cf2538af728b7f5a395929
unknown <jose.gamboa@labinfo.is.escuelaing.edu.co>
2022-09-07 19:18:41 -0500

    Arreglo

--- src/main/java/com/mycompany/microspring/MicroSpring.java
+++ src/main/java/com/mycompany/microspring/MicroSpring.java
@@ -38,7 +38,7 @@ public class MicroSpring {
         }
         ServerSocket serverSocket = null;
         try {
-            serverSocket = new ServerSocket(35000);
+            serverSocket = new ServerSocket(getPort());
         } catch (IOException e) {
             System.err.println("Could not listen on port: 35000.");
             System.exit(1);
@@ -91,5 +91,12 @@ public class MicroSpring {
             clientSocket.close();
         }
         serverSocket.close();
+    }
+    
+    static int getPort(){
+        if(System.getenv("PORT") != null){
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
     }
 }
